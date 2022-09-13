357f2987111508732e27a1f0e096c0d96d490b10
Clemens Horn <clemens.horn@mni.thm.de>
2022-09-13 23:59:10 +0200

    tauri builds in nx workspace
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/tauri-frontend/next.config.js
+++ apps/tauri-frontend/next.config.js
@@ -7,6 +7,15 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Note: This experimental feature is required to use NextJS Image in SSG mode.
+  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
